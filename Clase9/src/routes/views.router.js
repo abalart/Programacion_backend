6d4c727c30c964289ce8f08dceccb92a8d1ca54a
Agustin Balart <balartagustin@gmail.com>
2022-12-26 15:44:04 -0300

    Renderizo lista de productos

--- Clase9/src/routes/views.router.js
+++ Clase9/src/routes/views.router.js
@@ -1,21 +1,24 @@
 //En ese archivo tendremos la logica de renderizado, de donde tomar CSS,etc
 import express from 'express'
+import ProductManager from '../product_manager.js'
 
 const router = express.Router()
 
+const fileName = 'productos.json'
+const manager = new ProductManager(fileName)
+
+
 
 //Listado de productos
-
-
-
-
-router.get('/',(req,res)=>{
-    let prueba = {
-        name: 'efdgvdsfdv'
-    }
-    res.render('home',prueba)
+router.get('/',async (req,res)=>{
+   const list = await manager.getProducts()
+  
+    res.render('home',{
+        list
+    })
 
 })
+
 
 
 export default router
